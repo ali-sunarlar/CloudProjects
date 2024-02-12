dcad46c71b1ba4932163f30f292e1853ddff3483
Ali Sunarlar <sunarlarali@outlook.com>
2024-02-12 14:55:03 +0300

    new file:   Azure Infrastructure Solutions/Managing Route Table Using the PowerShell.ps1 modified:   Azure Infrastructure Solutions/Managing VNETs Using the PowerShell.ps1

---
+++ Azure Infrastructure Solutions/Managing Route Table Using the PowerShell.ps1
@@ -0,0 +1,45 @@
+New-AzResourceGroup -ResourceGroupName myResourceGroup -Location EastUS
+$routeTablePublic = New-AzRouteTable `
+  -Name 'myRouteTablePublic' `
+  -ResourceGroupName myResourceGroup `
+  -location EastUS
+
+  Get-AzRouteTable `
+  -ResourceGroupName "myResourceGroup" `
+  -Name "myRouteTablePublic" `
+  | Add-AzRouteConfig `
+  -Name "ToPrivateSubnet" `
+  -AddressPrefix 10.0.1.0/24 `
+  -NextHopType "VirtualAppliance" `
+  -NextHopIpAddress 10.0.2.4 `
+ | Set-AzRouteTable
+
+ $virtualNetwork = New-AzVirtualNetwork `
+  -ResourceGroupName myResourceGroup `
+  -Location EastUS `
+  -Name myVirtualNetwork `
+  -AddressPrefix 10.0.0.0/16
+
+  $subnetConfigPublic = Add-AzVirtualNetworkSubnetConfig `
+  -Name Public `
+  -AddressPrefix 10.0.0.0/24 `
+  -VirtualNetwork $virtualNetwork
+
+$subnetConfigPrivate = Add-AzVirtualNetworkSubnetConfig `
+  -Name Private `
+  -AddressPrefix 10.0.1.0/24 `
+  -VirtualNetwork $virtualNetwork
+
+$subnetConfigDmz = Add-AzVirtualNetworkSubnetConfig `
+  -Name DMZ `
+  -AddressPrefix 10.0.2.0/24 `
+  -VirtualNetwork $virtualNetwork
+
+  $virtualNetwork | Set-AzVirtualNetwork
+
+  Set-AzVirtualNetworkSubnetConfig `
+  -VirtualNetwork $virtualNetwork `
+  -Name 'Public' `
+  -AddressPrefix 10.0.0.0/24 `
+  -RouteTable $myRouteTablePublic | `
+Set-AzVirtualNetwork
--- Azure Infrastructure Solutions/Managing VNETs Using the PowerShell.ps1
+++ Azure Infrastructure Solutions/Managing VNETs Using the PowerShell.ps1
@@ -6,3 +6,6 @@ $vnet =Get-AzVirtualNetwork -name vnet2
 $vnet
 Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet | Select-Object name, AddressPrefix
 Get-Command *virtualnetwork*
+
+$subnet =New-AzVirtualNetworkSubnetConfig -Name Production -AddressPrefix 10.1.0.0/24
+New-AzVirtualNetwork -ResourceGroupName AZ300-RGlod37844694 -Location eastus2 -Name NewVNet -AddressPrefix 10.1.0.0/16 -Subnet $subnet
