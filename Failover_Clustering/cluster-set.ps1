6672b68fee98ed241098e27b442f015875d12f3b
Ali Sunarlar <sunarlarali@outlook.com>
2024-02-21 21:28:51 +0300

    deleted:    Azure_Lab_Terraform/main.tf deleted:    Azure_Lab_Terraform/output.tf deleted:    Azure_Lab_Terraform/providers.tf deleted:    ContainerLab/acr.md deleted:    ContainerLab/docker.azcli new file:   Failover_Clustering/cluster-set.ps1 new file:   Failover_Clustering/clustering.ps1 deleted:    Kubernetes/aks.azcli deleted:    Kubernetes/kubernetes.azcli deleted:    Kubernetes/managed-premium.yaml deleted:    Kubernetes/nginx-deployment.yaml deleted:    Kubernetes/nginx-privileged.yaml deleted:    Kubernetes/persistent-volume-claim-5g.yaml deleted:    Kubernetes/persistent-volume-claim.yaml deleted:    Kubernetes/pod-with-storage-mount.yaml deleted:    Kubernetes/premium-storage-class.yaml

---
+++ Failover_Clustering/cluster-set.ps1
@@ -0,0 +1,48 @@
+# Cluster Sets
+
+Ref: https://timw.info/7kc
+
+# Create a cluster set
+New-ClusterSet -Name CSMASTER -NamespaceRoot SOFS-CLUSTERSET -CimSession SET-CLUSTER
+
+# Add cluster members to cluster set
+Add-ClusterSetMember -ClusterName CLUSTER1 -CimSession CSMASTER -InfraSOFSName SOFS-CLUSTER1
+
+Add-ClusterSetMember -ClusterName CLUSTER2 -CimSession CSMASTER -InfraSOFSName SOFS-CLUSTER2
+
+# Enumerate all member clusteres including the management nodes
+Get-ClusterSet -CimSession CSMASTER | Get-Cluster | Get-ClusterNode
+
+# Add management cluster to the local Administrators group on each cluster member server node in the cluster set
+foreach ($h in $hosts) { Invoke-Command -ComputerName $h -ScriptBlock { Net localgroup administrators /add <management_cluster_name>$ } }
+
+# Identify the optimal node to create a new virtual machine
+$memoryinMB = 4096
+$vpcount = 1
+$targetnode = Get-ClusterSetOptimalNodeForVM -CimSession CSMASTER -VMMemory $memoryinMB -VMVirtualCoreCount $vpcount -VMCpuReservation 10
+$secure_string_pwd = convertto-securestring "<password>" -asplaintext -force
+$cred = new-object -typename System.Management.Automation.PSCredential ("<domain\account>", $secure_string_pwd)
+
+# Deploy the virtual machine on the optimal node
+Invoke-Command -ComputerName $targetnode.name -scriptblock { param([String]$storagepath); New-VM CSVM1 -MemoryStartupBytes 3072MB -path $storagepath -NewVHDPath CSVM.vhdx -NewVHDSizeBytes 4194304 } -ArgumentList @("\\SOFS-CLUSTER1\VOLUME1") -Credential $cred | Out-Null
+
+Start-VM CSVM1 -ComputerName $targetnode.name | Out-Null
+
+Get-VM CSVM1 -ComputerName $targetnode.name | fl State, ComputerName
+
+Register-ClusterSetVM -CimSession CSMASTER -MemberName $targetnode.Member -VMName CSVM1
+
+# Add SoFS role to the cluster
+Add-ClusterScaleoutFileServerRole -Name "my_infra_sofs_name" -Infrastructure
+
+# Create fault domains
+New-ClusterSetFaultDomain -Name FD1 -FdType Logical -CimSession CSMASTER -MemberCluster CLUSTER1, CLUSTER2 -Description "First fault domain"
+
+New-ClusterSetFaultDomain -Name FD2 -FdType Logical -CimSession CSMASTER -MemberCluster CLUSTER3, CLUSTER4 -Description "Second fault domain"
+
+# Create availability set
+New-ClusterSetAvailabilitySet -Name CSMASTER-AS -FdType Logical -CimSession CSMASTER -ParticipantName FD1, FD2
+
+# Remove a cluster from the cluster set
+Remove-ClusterSetMember -ClusterName CLUSTER1 -CimSession CSMASTER
+
---
+++ Failover_Clustering/clustering.ps1
@@ -0,0 +1,37 @@
+# Failover Clustering
+
+# Install the role
+# Fill in these variables with your values
+#$ServerList = "server2.contoso.com", "server3.contoso.com"
+$ServerList="DEMOCLS1.demo.local", "DEMOCLS2.demo.local"
+#$FeatureList = "Hyper-V", "Failover-Clustering", "Data-Center-Bridging", "RSAT-Clustering-PowerShell", "Hyper-V-PowerShell", "FS-FileServer"
+$FeatureList = "Failover-Clustering","RSAT-Clustering-PowerShell", "FS-FileServer"
+Invoke-Command ($ServerList) {
+  Install-WindowsFeature -Name $Using:Featurelist -Restart
+}
+
+# Validate the cluster
+Test-Cluster -Node $ServerList -Include "Storage Spaces Direct", "Inventory", "Network", "System Configuration"
+
+# Create the cluster
+New-Cluster -Name "vmcluster" -Node $ServerList -NoStorage -StaticAddress 192.168.86.123
+
+# Create cloud witness
+Set-ClusterQuorum -CloudWitness -AccountName "account-name" -AccessKey ""
+
+# Enable S2D
+Enable-ClusterStorageSpacesDirect -CimSession "az801cluster" -PoolFriendlyName "S2D-Pool1"
+
+# Enable CSV cache
+$ClusterName = "vmcluster1"
+$CSVCacheSize = 2048 #Size in MB
+
+Write-Output "Setting the CSV cache..."
+(Get-Cluster $ClusterName).BlockCacheSize = $CSVCacheSize
+
+$CSVCurrentCacheSize = (Get-Cluster $ClusterName).BlockCacheSize
+Write-Output "$ClusterName CSV cache size: $CSVCurrentCacheSize MB"
+
+Get-PhysicalDisk | Select-Object -Property FriendlyName, ClassName, CanPool, CannotPoolReason | Format-Table -AutoSize
+
+Get-PhysicalDisk | Format-List *
